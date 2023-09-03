edbb07a07d1705728301df1f55025c95e623e9bc
Olamide-f <mfavored1@gmail.com>
2023-09-03 19:01:36 -0400

    initial commit

--- my-detection.py
+++ my-detection.py
@@ -1,11 +1,10 @@
-import jetson.inference
-import jetson.utils
+import jetson_inference
+import jetson_utils
 
 import argparse
 import sys
 
-from jetson_utils import videoSource, videoOutput
-
+from jetson_utils import videoSource, videoOutput, cudaFont
 
 # parse the command line
 parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.")
@@ -23,21 +22,29 @@ except:
 	parser.print_help()
 	sys.exit(0)
 
+
 # load the object detection network
-net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
+net = jetson_inference.detectNet(
+    model="ssd-mobilenet",
+    labels="labels.txt",
+    input_blob="input_0",
+    output_cvg="scores",
+    output_bbox="boxes",
+    threshold=0.5
+)
+
+
 
 # create video sources & outputs
 input = videoSource("/dev/video0")
-output = videoOutput("rtsp://@:1234/my_output")
+output = videoOutput("display://0")
 
-###
-#trying qmessagebox pop-up messages
-# from PyQt5 import QtCore, QtGui, QtWidgets
-# from PyQt5.QtWidgets import QMessageBox
+font = cudaFont()
 
 ###
 # process frames until the user exits
 while True:
+	###### sg.popup('Basic Pop-up', 'And it can show multiline text and variables')
 	# capture the next image
 	img = input.Capture()
 
@@ -47,8 +54,42 @@ while True:
 	# print the detections
 	print("detected {:d} objects in image".format(len(detections)))
 
+	dish_count = 0
 	for detection in detections:
-		print(detection)
+		#print(detection)
+		label = net.GetClassLabel(detection.ClassID)
+		print(label)
+		if label == "bottle" or label == "bottle opener" or label == "bowl" or label == "coffee cup" or label == "countertop" or label == "dishwasher" or label == "drink" or label == "drinking straw":
+			dish_count += 1
+		elif label == "fork" or label == "juice" or label == "kitchen appliance" or label == "kitchen knife" or label == "kitchen utensil" or label == "knife" or label == "measuring cup" or label == "mixing bowl":
+			dish_count += 1
+		elif label == "mug" or label == "soap dispenser" or label == "wine glass" or label=="cup" or label=="spoon":
+			dish_count += 1
+		
+
+		if label == "bottle" or label == "bottle opener" or label == "bowl" or label == "coffee cup" or label == "countertop" or label == "dishwasher" or label == "drink" or label == "drinking straw":
+			font.OverlayText(img, text="Uh oh! There are {:d} dishes in the sink. Let's clean them up quickly!!".format(dish_count), 
+            	x=200, y=100,
+            	color=font.White, background=font.Blue)
+		elif label == "fork" or label == "juice" or label == "kitchen appliance" or label == "kitchen knife" or label == "kitchen utensil" or label == "knife" or label == "measuring cup" or label == "mixing bowl":
+			font.OverlayText(img, text="Uh oh! There are {:d} dishes in the sink. Let's clean them up quickly!!".format(dish_count), 
+            	x=200, y=100,
+            	color=font.White, background=font.Blue)
+		elif label == "mug" or label == "soap dispenser" or label == "wine glass" or label=="cup" or label=="spoon":
+			font.OverlayText(img, text="Uh oh! There are {:d} dishes in the sink. Let's clean them up quickly!!".format(dish_count), 
+            	x=200, y=100,
+            	color=font.White, background=font.Blue)
+		else:
+			font.OverlayText(img, text="Lucky you! There are no dishes in the sink.", 
+            	x=200, y=100,
+            	color=font.White, background=font.Black)
+	if len(detection) == 0:
+		font.OverlayText(img, text="Lucky you! There are no dishes in the sink.", 
+    		x=200, y=100,
+        	color=font.White, background=font.Black)
+	
+	dish_count = 0
+	###############
 
 	# render the image
 	output.Render(img)
